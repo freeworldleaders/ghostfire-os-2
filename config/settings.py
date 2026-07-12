ce5f16b5a3a7521648b7850242ab091b8fe41837
GhostFire Commander <YOUR_EMAIL@gmail.com>
2026-07-11 22:27:55 -0500

    Add configuration system

---
+++ config/settings.py
@@ -0,0 +1,5 @@
+SETTINGS = {
+    "app_name": "Ghostfire OS",
+    "version": "0.2.0",
+    "runtime": "online"
+}
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from config.settings import SETTINGS
 from runtime.engine import RuntimeEngine
 from router.router import CommandRouter
 from agents.registry import AgentRegistry
+
+print(f"{SETTINGS['app_name']} {SETTINGS['version']}")
 
 RuntimeEngine().start()
 
